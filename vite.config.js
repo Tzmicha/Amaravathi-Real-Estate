28e2c7e47419e6969782cfd54b725be101af1692
DANI EVENS <danievens78@gmail.com>
2026-06-01 14:24:54 +0530

    Update Vite config URL in comment
     Folder structure is in ROOT - for viTe proJect -  Base is needed when hosting a. VITE app inside - GITHUT REPO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev/configure/
 export default defineConfig({
   base: '/Amaravathi-Real-Estate/',
   plugins: [react()],
